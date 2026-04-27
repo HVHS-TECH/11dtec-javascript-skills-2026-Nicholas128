a0304b01896091d44f7a88ad7fe4f73fb7ff86be
Nicholas128 <24049nm@hvhs.school.nz>
2026-04-27 22:30:47 +0000

    Apr 27, 2026, 10:30 PM

--- t02_variables.js
+++ t02_variables.js
@@ -8,10 +8,10 @@ Name of task: Commenting on code
  console.log("as of"+ currentYear) 
 //Variables
 var name = " Nicholas"
-var age = 15;
+let age = 15;
 var isStudent = true;
 var pocketMoney = "$400"
-var currentYear = 2025;
+let currentYear = 2025;
 /******************
  Main code
 *******************/
