e37d4aaa9ff7ede23a3939ebd572619647d3981e
Nicholas128 <24049nm@hvhs.school.nz>
2026-04-27 22:57:22 +0000

    Apr 27, 2026, 10:57 PM

--- t03_maths.js
+++ t03_maths.js
@@ -10,8 +10,10 @@ var name = " Nicholas"
 var currentYear = 2025;
 var age = 15;
 var number15 = 15;
+var answer;
 var isStudent = true;
 var pocketMoney = "$400"
+
 
 
 /******************
@@ -22,7 +24,8 @@ console.log( "hello world");
 console.log( "hi"+ name)
 console.log( "as of "+ currentYear) 
 console.log("you are "+ age + " years old")
-console.log( "you were born in"+ age - number15)
+answer = age - number15;
+console.log( "you were born in"+ answer)
 console.log("you have "+ pocketMoney)
 
 
