3384baafd1065df876c14a02f8dade59abe9d6a9
Nicholas128 <24049nm@hvhs.school.nz>
2026-04-27 22:42:11 +0000

    Apr 27, 2026, 10:42 PM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,28 @@
-/* Paste your code from task 2 here */
+/******************
+Name of task: Maths
+******************/
+
+ 
+ 
+
+//Variables
+var name = " Nicholas"
+var currentYear = 2025;
+var age = 15;
+var isStudent = true;
+var pocketMoney = "$400"
+
+/******************
+ Main code
+*******************/
+console.log(  "Running T1-intro.js");
+console.log( "hello world");
+console.log( "hi"+ name)
+console.log( "as of "+ currentYear) 
+console.log("you are "+ age + " years old")
+console.log("you have "+ pocketMoney)
+
+
+/*******************
+Functions
+*******************/
