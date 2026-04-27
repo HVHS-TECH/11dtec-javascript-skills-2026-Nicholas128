fdc7e013dd7c6530120c7f25a6f3a81dce178ff1
Nicholas128 <24049nm@hvhs.school.nz>
2026-04-27 22:52:59 +0000

    Apr 27, 2026, 10:52 PM

--- t03_maths.js
+++ t03_maths.js
@@ -9,8 +9,10 @@ Name of task: Maths
 var name = " Nicholas"
 var currentYear = 2025;
 var age = 15;
+var number15 = 15;
 var isStudent = true;
 var pocketMoney = "$400"
+
 
 /******************
  Main code
@@ -19,6 +21,7 @@ console.log(  "Running T1-intro.js");
 console.log( "hello world");
 console.log( "hi"+ name)
 console.log( "as of "+ currentYear) 
+console.log( "you were born in"+ age - number15)
 console.log("you are "+ age + " years old")
 console.log("you have "+ pocketMoney)
 
