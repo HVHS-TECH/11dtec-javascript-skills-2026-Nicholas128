8473b5065a851098ad77983a6829d12dd3dfc3f5
Nicholas128 <24049nm@hvhs.school.nz>
2026-04-27 22:32:01 +0000

    Apr 27, 2026, 10:32 PM

--- t02_variables.js
+++ t02_variables.js
@@ -5,7 +5,7 @@ Name of task: Commenting on code
  console.log(  "Running T1-intro.js");
  console.log( "hello world");
  console.log( "hi"+ name)
- console.log("as of"+ currentYear) 
+ console.log( "as of"+ currentYear) 
 //Variables
 var name = " Nicholas"
 let age = 15;
