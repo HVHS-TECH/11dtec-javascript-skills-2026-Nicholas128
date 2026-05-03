1b424d19d02474ddb34127696b06d15c227df7cc
Nicholas128 <24049nm@hvhs.school.nz>
2026-05-03 21:29:31 +0000

    May 3, 2026, 9:29 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,42 @@
-/* Paste your code from task 3, Maths here */
+/******************
+Name of task: JavaScript and HTML
+******************/
+
+ 
+ 
+
+//Variables
+var name = " Nicholas"
+var currentYear = 2025;
+var age = 15;
+var number15 = 15;
+var number10 = 10; 
+var answer;
+var answer2;
+var isStudent = true;
+var pocketMoney = "400" 
+var number200 = 200;
+var answer3;
+
+
+/******************
+ Main code
+*******************/
+console.log(  "Running T1-intro.js");
+console.log( "hello world");
+console.log( "hi"+ name)
+console.log( "as of "+ currentYear) 
+console.log("you are "+ age + " years old")
+answer = currentYear - number15;
+console.log( "you were born in "+ answer)
+answer2 = age + number10;
+console.log( "in 10 years you will be " + answer2)
+console.log("you have $"+ pocketMoney)
+answer3 = pocketMoney - number200;
+console.log( "You spend half of your money, now you have $" + answer3)
+
+
+
+/*******************
+Functions
+*******************/
