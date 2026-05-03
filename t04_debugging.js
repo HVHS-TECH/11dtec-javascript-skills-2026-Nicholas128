fbf0b437b6620839e6aaa998d6b3ce8a9066e577
Nicholas128 <24049nm@hvhs.school.nz>
2026-05-03 21:12:47 +0000

    May 3, 2026, 9:12 PM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,14 +1,15 @@
 console.log("Task 4 - Debugging")
 
+// Set up the size of the page.
+var length = 8;
+var width = 10;
+
 // calculate the area
 var area = length * width;
 
 // calculate the perimeter
 var perimeter = length + width; 
 
-// Set up the size of the page.
-var length = 8;
-var width = 10;
 
 // Display the results
 console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area )
