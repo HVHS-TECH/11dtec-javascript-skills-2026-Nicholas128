411f2d176530a4547a0f4c3c79b8dcdaf2285bd0
Nicholas128 <24049nm@hvhs.school.nz>
2026-05-04 22:54:03 +0000

    May 4, 2026, 10:54 PM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,63 @@
-/* Paste your code from task 5 here */
+/******************
+Name of task: Functions
+******************/
+
+ 
+ 
+
+//Variables
+var name = " Nicholas"
+var currentYear = 2026;
+var age = 15;
+var number16 = 16;
+var number10 = 10; 
+var answer;
+var answer2;
+var isStudent = true;
+var pocketMoney = "400" 
+var number200 = 200;
+var answer3;
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+OUTPUT.innerHTML="<p>Hello world</p>"; 
+OUTPUT.innerHTML+="<p>Hello" + name + "</p>";
+OUTPUT.innerHTML+="<p>As of " +currentYear+  "</p>"
+OUTPUT.innerHTML+="<p>You are " + age + " years old </p>";
+answer = currentYear - number16;
+OUTPUT.innerHTML+="<p>You were born in " + answer + "</p>";
+answer2 = age + number10;
+OUTPUT.innerHTML+="<p>In 10 years you will be " + answer2 + "</p>"
+OUTPUT.innerHTML+="<p>You have $" + pocketMoney + "</p>"
+answer3 = pocketMoney - number200;
+OUTPUT.innerHTML+="<p>You spend half of your money, now you have $" + answer3 + "</p>";
+
+/******************
+ Functions
+*******************/
+function writeLine(){
+    OUTPUT.innerHTML += "<p>Less code matters</p>";
+}
+/******************
+ Main code
+*******************/
+
+
+console.log(  "Running T5-Functions");
+console.log( "hello world");
+console.log( "hi"+ name)
+console.log( "as of "+ currentYear) 
+console.log("you are "+ age + " years old")
+answer = currentYear - number16;
+console.log( "you were born in "+ answer)
+answer2 = age + number10;
+console.log( "in 10 years you will be " + answer2)
+console.log("you have $"+ pocketMoney)
+answer3 = pocketMoney - number200;
+console.log( "You spend half of your money, now you have $" + answer3)
+writeLine()
+
+
+/*******************
+Functions
+*******************/
