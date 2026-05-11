b74ef9f069c371be21c793d95190b50bd8b7bc97
Nicholas128 <24049nm@hvhs.school.nz>
2026-05-11 22:38:12 +0000

    May 11, 2026, 10:38 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -28,7 +28,7 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 function getFormInput() {
 const NAME_FIELD = document.getElementById("nameField");
 var userName = NAME_FIELD.value;
-OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+OUTPUT.innerHTML += "<p>Your name is " + userName + "</p>";
 }
 
 
