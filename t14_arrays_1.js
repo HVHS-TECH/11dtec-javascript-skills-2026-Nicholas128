64bb1466eead109591e0b28a34c5725c7adf2427
Nicholas128 <24049nm@hvhs.school.nz>
2026-05-24 21:48:48 +0000

    May 24, 2026, 9:48 PM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -21,7 +21,10 @@ var number4 = 4;
 var number3 = 3;
 var number250 = 2.50;
 var classArray = ["0", "1", "2", "3"];
-OUTPUT.innterHTML = "You loath chocolate"+
+OUTPUT.innerHTML = "You loath chocolate"+classArray[0];
+OUTPUT.innerHTML = "Chocolate is meh"+classArray[1];
+OUTPUT.innerHTML = "Chocolate is pretty good"+classArray[2];
+
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 function getNameInput() {
