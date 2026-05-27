28d654f62da377850477acf0a561cb64b833c7fb
Nicholas128 <24049nm@hvhs.school.nz>
2026-05-27 00:56:25 +0000

    May 27, 2026, 12:56 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -165,11 +165,10 @@ OUTPUT.innerHTML += "<p>You will get $" + change + " change</p>";
     var classArray = ["0", "1", "2", "3"];
     var choice = CHOOSE_FIELD.value;
     OUTPUT.innerHTML += classArray;
-    OUTPUT.innerHTML += "You chose: "+classArray[choice]+"<br>";
     OUTPUT.innerHTML += "You loath chocolate"+classArray[0];
     OUTPUT.innerHTML += "Chocolate is meh"+classArray[1];
     OUTPUT.innerHTML += "Chocolate is pretty good"+classArray[2];
     OUTPUT.innerHTML += "Chocolate is the best thing EVER!!!"+classArray[3];
-   
+    OUTPUT.innerHTML += "You chose: "+classArray[choice]+"<br>";
 }
 
