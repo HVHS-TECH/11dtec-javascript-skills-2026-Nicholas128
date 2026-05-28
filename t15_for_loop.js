695bd3a86c2d7ea06aa8fb0b73eaa019586eda3a
Nicholas128 <24049nm@hvhs.school.nz>
2026-05-28 02:33:44 +0000

    May 28, 2026, 2:33 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -183,8 +183,9 @@ if (choice == 0) {
 function number() {
 const NUMBER_FIELD = document.getElementById("numberField");  
 console.log("Starting loop");
-for (count = 0; count < 99; count++)
+for (count = 0; count < 99; count++) {
     console.log("current count; " + count);
 }
-
 console.log("loop stopped");
+}
+
