e71b644927dafd7781e7adda97d4750c0d3895d9
Nicholas128 <24049nm@hvhs.school.nz>
2026-06-01 04:11:40 +0000

    Jun 1, 2026, 4:11 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -216,7 +216,8 @@ function addShoppingItem() {
 
   const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
-  OUTPUT.innerHTML = "<p>Out of eggs, butter, milk and wheat, what would you like to add to the shopping list?</p>";
+OUTPUT.innerHTML = "<p>Out of eggs, butter, milk and wheat, what would you like to add to the shopping list?</p>";
+return;
 
   var classArray = ["eggs", "butter", "milk", "wheat"];
 
